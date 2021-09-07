8770d5174f144d7abb5734d46abc5d460d72eb94
Sam Thabet <sallam.thabet@gmail.com>
2021-09-06 22:56:47 -0400

    added the push freature to save items into db.json

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+const uniqid = require("uniqid");
 
 router.post("/api/notes", (req, res) => {
   const data = JSON.parse(
     fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
   );
-  // find an id maker
-
+  req.body.id = uniqid();
+  //push object into data
+  data.push(req.body);
   //write file
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(data));
+  res.json(req.body);
 });
 
 router.get("/api/notes", (req, res) => {
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const htmlRoutes = require("./routes/html");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
 app.use(express.json());
+//lesson 11.3.4
 app.use(express.static("public"));
 
 app.use(apiRoutes);
